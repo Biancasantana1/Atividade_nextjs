10d896744a7e1baf6a3e1799fb848eb800a05012
Bianca Santana <bianca.santana@toodoo.com.br>
2023-05-15 18:03:56 -0300

    Subindo att

--- app/page.js
+++ app/page.js
@@ -28,7 +28,7 @@ function ProductRow({ product }) {
 function ProductTable({ blogMessages, filterText}) {
   const rows = [];
   blogMessages.forEach((product) => {
-    if (String (product[0]).toLowerCase().indexOf(filterText.toLowerCase()) === -1) {
+    if (String (product[0]).toLowerCase().indexOf(filterText.toLowerCase()) >= 0) {
       return;
     }
     rows.push(
