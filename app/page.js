b948d975f203e4608e5787a9e2f35b28a08c9604
Bianca Santana <bianca.santana@toodoo.com.br>
2023-05-15 23:30:22 -0300

    Subindo att

--- app/page.js
+++ app/page.js
@@ -4,7 +4,6 @@ import Image from 'next/image'
 import styles from './page.module.css'
 //import React from 'react';
 //import { useState } from 'react';
-import moment from 'moment';
 import React, { useState, useEffect } from 'react';
 
 function SearchBar({filterText, onFilterTextChange}) {
@@ -18,13 +17,21 @@ function SearchBar({filterText, onFilterTextChange}) {
 }
 
 function ProductRow({ product }) {
-  const date = moment(product[2]).format('DD/MM/YYYY HH:mm:ss');
+  const date = new Date(product[2]);
+  const day = ("0" + date.getUTCDate()).slice(-2);
+  const month = ("0" + (date.getUTCMonth()+1)).slice(-2);
+  const year = date.getUTCFullYear();
+  const hours = ("0" + date.getUTCHours()).slice(-2);
+  const minutes = ("0" + date.getUTCMinutes()).slice(-2);
+  const seconds = ("0" + date.getUTCSeconds()).slice(-2);
+
+  const formattedDate = `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`;
 
   return (
     <tr>
       <td>{product[1]}</td>
       <td>{product[0]}</td>
-      <td>{date}</td>
+      <td>{formattedDate}</td>
     </tr>
   );
 }
